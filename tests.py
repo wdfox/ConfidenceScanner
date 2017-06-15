49d36d64116f564e0e5e4d99965d3b6906a4c6da
Will Fox <wdfox@mit.edu>
2017-06-14 23:09:53 -0700

    Small changes

--- tests.py
+++ tests.py
@@ -7,26 +7,26 @@ import urls
 
 # PubMed
 
-search = urls.build_search("aging")
-ids = urls.get_ids(search)
-print(ids)
+# search = urls.build_search("aging")
+# ids = urls.get_ids(search)
+# print(ids)
 
-fetch = urls.build_fetch(ids[0])
-print(fetch)
+# fetch = urls.build_fetch(ids[0])
+# print(fetch)
 
-paper = base.Paper(ids[0])
-print("Paper id is", paper.id)
+# paper = base.Paper(ids[0])
+# print("Paper id is", paper.id)
 
-paper.scrape_data()
-print("Paper title is", paper.title)
-print("Paper author(s):", paper.authors)
-print("Paper journal:", paper.journal)
-print("Paper year:", paper.year)
-print("Paper abstract: \n", paper.text)
+# paper.scrape_data()
+# print("Paper title is", paper.title)
+# print("Paper author(s):", paper.authors)
+# print("Paper journal:", paper.journal)
+# print("Paper year:", paper.year)
+# print("Paper abstract: \n", paper.text)
 
 
 
-# # NIH Database
+# NIH Database
 
 # pr = base.Press_Release("https://www.nih.gov/news-events/news-releases/pregnancy-diet-high-refined-grains-could-increase-child-obesity-risk-age-7-nih-study-suggests")
 
@@ -39,7 +39,7 @@ print("Paper abstract: \n", paper.text)
 
 # NIH Crawler
 
-# base.crawl()
+urls.crawl()
 
 
 
@@ -47,12 +47,20 @@ print("Paper abstract: \n", paper.text)
 
 
 """
+test for funny characters using this article 'https://www.nih.gov/news-events/news-releases/researchers-aim-repurpose-former-experimental-cancer-therapy-treat-muscular-dystrophy'
+and paper with url: 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=28609678'
+
 
 save in json (use dictionary) --> look at erp_data.py for save and load methods
 
-Search terms: 
-	NINDS, NIAAA also good databases to look through (check against NIH main database, also check for overlap)
-	NINDS - Stroke
-	NIAAA - alcohol abuse, alcoholism
+
+TO DO:
+
+1) Save and load scraped info in json files
+2) Check how funny characters get handled
+3) Create method for analysis
+4) Create run script for mass paper collection
+5) Write function to cycle through NIH website and collect urls
+6) Create run script for mass pr collection
 
 """
