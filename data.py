9ecec315fe329a2e4bdbfcf9490922c69e927e4d
Will Fox <wdfox@mit.edu>
2017-06-26 09:40:06 -0500

    Updates to data collection function

--- data.py
+++ data.py
@@ -1,13 +1,40 @@
 ''' Functions for saving and loading data into json file format '''
 
 import json
+import os
 
 
-def save(type_of_data, data_to_write, where_to_write_it_to):
-	with open('something_about_data.json', 'w') as outfile:
-		json.dump(data, outfile)
-		outfile.write('\n')
+# NOT SUPER SURE HOW PORTABLE THIS PART OF THE CODE IS BETWEEN USERS--not that it's really that important anyway
+def save_location(search_type, search_term):
+	
+	# Search term refers to a specific keywork if search_type is paper or a specific pr database if the type is pr
+    base = 'Users/wdfox/Documents/GitCode/Confidence_Scanner/Data/'
+    path = base + str(search_type) + str(search_term)
+
+    if not os.path.isdir(path):
+        os.mkdirs(path)
+
+    return(path)
+
+    # FINISH FIGURING OUT WHERE TO SAVE THE DIFFERENT FILES, THEN IMPLEMENT INTO THE RUN_SCRIPT
+
+
+
+
+def save(type_of_data, data, search_term):
+    
+    
+    # Need to get the data into the right format to be saved first
+
+    path = save_location(type_of_data, search_term)
+
+    # Convert our information into a savable dictionary format
+    info_dict = data.__dict__
+
+    with open(path + '.json', 'w') as outfile:
+        json.dump(info_dict, outfile)
+        outfile.write('\n')
 
 
 def load():
-	pass
+    pass
