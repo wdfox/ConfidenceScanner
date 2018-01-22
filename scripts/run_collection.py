ef40c10b14705cdde59a9926bbd1d336372dba0a
TomDonoghue <tdonoghue@ucsd.edu>
2018-01-21 18:03:31 -0800

    Add scripts

---
+++ scripts/run_collection.py
@@ -0,0 +1,28 @@
+"""Script to run a collection."""
+
+import datetime
+from consc.collection import collect_papers, collect_prs
+
+###################################################################################################
+###################################################################################################
+
+# Set search terms
+TERMS = ['dementia', 'autism']
+COUNT = 500
+
+# Set the start & end date
+START_DATE = datetime.date(year=2017, month=1, day=1)
+END_DATE = datetime.date(year=2017, month=12, day=31)
+
+###################################################################################################
+###################################################################################################
+
+def main():
+
+    # Collect papers & press-releases
+    for term in TERMS:
+        collect_papers(paper_count=str(COUNT), search_term=term, use_hist=True)
+        collect_prs(search_term=term, START_DATE, END_DATE, pr_count=COUNT)
+
+if __name__ == '__main__':
+    main()
