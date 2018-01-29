f2977e20467dd795f8209e6ceeb55cc8da80037a
TomDonoghue <tdonoghue@ucsd.edu>
2018-01-28 20:49:23 -0800

    Update file IO

--- consc/analysis/confidence.py
+++ consc/analysis/confidence.py
@@ -1,32 +1,48 @@
-''' LIWC Confidence Method '''
+"""LIWC Confidence Method"""
 
-high_con_file = open('../corpus/positive.txt', 'r')
-high_con_words = high_con_file.read().splitlines()
+import pkg_resources as pkg
+from consc.data import load_folder
 
-low_con_file = open('../corpus/negative.txt', 'r')
-low_con_words = low_con_file.read().splitlines()
+##
+##
+
+def load_corpus_words(file_name):
+    """   """
+
+    f_name = 'corpus/' + file_name + '.txt'
+    f_path = pkg.resource_filename('consc', f_name)
+
+    corpus_file = open(f_path, 'r')
+    corpus_words = corpus_file.read().splitlines()
+
+    return corpus_words
+
+
+HIGH_CON_WORDS = load_corpus_words('positive')
+LOW_CON_WORDS = load_corpus_words('negative')
+
 
 def doc_confidence(document):
 
-	confidence = 0
+    confidence = 0
 
-	for i in document.words:
+    for i in document.words:
 
-		for j in high_con_words:
-			if j in i:
-				confidence += 1
+        for j in HIGH_CON_WORDS:
+            if j in i:
+                confidence += 1
 
-		for k in low_con_words:
-			if k in i:
-				confidence -= 1
+        for k in LOW_CON_WORDS:
+            if k in i:
+                confidence -= 1
 
-	return confidence
+    return confidence
 
 
 def folder_confidence(data_type, search_term):
 
-	docs = load_folder(data_type, search_term)
+    docs = load_folder(data_type, search_term)
 
-	confidence = [doc_confidence(doc) for doc in docs]
+    confidence = [doc_confidence(doc) for doc in docs]
 
-	return confidence
+    return confidence
