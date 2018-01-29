a110910ff120e40e41aa2fd8a9425894a9b095f1
Will Fox <wdfox@mit.edu>
2018-01-29 15:03:05 -0500

    Modifications for running specific scrapes

--- scripts/run_collection.py
+++ scripts/run_collection.py
@@ -10,15 +10,12 @@ from consc.collection import collect_papers, collect_prs
 # Set search terms
 #TERMS = ['dementia', 'autism']
 #COUNT = 500
-# TERMS = ['aging']
-# COUNT = 30
-# TERMS = ['autism', 'dementia', 'epilepsy', 'stroke', 'parkinsons', 'optogenetics', 'bilingualism', 'consciousness', 'perception', 'cognition', 'vaccines', 'coma', 'diabetes', 'hypertension']
-TERMS = ['consciousness', 'perception', 'cognition', 'vaccines', 'coma', 'diabetes', 'hypertension']
+TERMS = ['autism', 'dementia', 'epilepsy', 'stroke', 'parkinsons', 'optogenetics', 'bilingualism', 'consciousness', 'perception', 'cognition', 'vaccines', 'coma', 'diabetes', 'hypertension']
 COUNT = 1000
 
 # Set the start & end date
-# START_DATE = datetime.date(year=2017, month=1, day=1)
-# END_DATE = datetime.date(year=2017, month=1, day=14)
+# START_DATE = datetime.date(year=2017, month=5, day=7)
+# END_DATE = datetime.date(year=2017, month=12, day=31)
 START_DATE = datetime.date(year=2017, month=1, day=1)
 END_DATE = datetime.date(year=2017, month=12, day=31)
 
@@ -31,8 +28,8 @@ def main():
 
 	# Collect papers & press-releases
 	for term in TERMS:
-		# collect_papers(search_term=term, start_date=START_DATE, end_date=END_DATE, paper_count=COUNT, use_hist=True)
-		# print(term, 'papers collected')
+		collect_papers(search_term=term, start_date=START_DATE, end_date=END_DATE, paper_count=COUNT, use_hist=True)
+		print(term, 'papers collected')
 		collect_prs(search_term=term, start_date=START_DATE, end_date=END_DATE, driver=driver, pr_count=COUNT)
 		print(term, 'prs collected')
 
