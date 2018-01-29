9af19d7621fe598ea02c5d4804991fd2c137ab5e
Will Fox <wdfox@mit.edu>
2018-01-29 12:17:55 -0500

    Updates for scraping procedure

--- scripts/run_collection.py
+++ scripts/run_collection.py
@@ -1,6 +1,7 @@
 """Script to run a collection."""
 
 import datetime
+from selenium import webdriver
 from consc.collection import collect_papers, collect_prs
 
 ###################################################################################################
@@ -9,24 +10,34 @@ from consc.collection import collect_papers, collect_prs
 # Set search terms
 #TERMS = ['dementia', 'autism']
 #COUNT = 500
-TERMS = ['aging']
-COUNT = 30
+# TERMS = ['aging']
+# COUNT = 30
+# TERMS = ['autism', 'dementia', 'epilepsy', 'stroke', 'parkinsons', 'optogenetics', 'bilingualism', 'consciousness', 'perception', 'cognition', 'vaccines', 'coma', 'diabetes', 'hypertension']
+TERMS = ['consciousness', 'perception', 'cognition', 'vaccines', 'coma', 'diabetes', 'hypertension']
+COUNT = 1000
 
 # Set the start & end date
+# START_DATE = datetime.date(year=2017, month=1, day=1)
+# END_DATE = datetime.date(year=2017, month=1, day=14)
 START_DATE = datetime.date(year=2017, month=1, day=1)
-END_DATE = datetime.date(year=2017, month=1, day=14)
-#START_DATE = datetime.date(year=2017, month=1, day=1)
-#END_DATE = datetime.date(year=2017, month=12, day=31)
+END_DATE = datetime.date(year=2017, month=12, day=31)
 
 ###################################################################################################
 ###################################################################################################
 
 def main():
 
-    # Collect papers & press-releases
-    for term in TERMS:
-        collect_papers(search_term=term, start_date=START_DATE, end_date=END_DATE, paper_count=COUNT, use_hist=True)
-        #collect_prs(search_term=term, start_date=START_DATE, end_date=END_DATE, pr_count=COUNT)
+	driver = webdriver.Safari()
+
+	# Collect papers & press-releases
+	for term in TERMS:
+		# collect_papers(search_term=term, start_date=START_DATE, end_date=END_DATE, paper_count=COUNT, use_hist=True)
+		# print(term, 'papers collected')
+		collect_prs(search_term=term, start_date=START_DATE, end_date=END_DATE, driver=driver, pr_count=COUNT)
+		print(term, 'prs collected')
+
+	driver.close()
+
 
 if __name__ == '__main__':
-    main()
+	main()
