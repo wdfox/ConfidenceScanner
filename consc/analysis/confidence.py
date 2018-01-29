f669c758c0883b501a61a26ff5b50864b2bb0ca7
TomDonoghue <tdonoghue@ucsd.edu>
2018-01-28 21:10:02 -0800

    Add option to normalize by length

--- consc/analysis/confidence.py
+++ consc/analysis/confidence.py
@@ -22,7 +22,7 @@ HIGH_CON_WORDS = load_corpus_words('positive')
 LOW_CON_WORDS = load_corpus_words('negative')
 
 
-def doc_confidence(document):
+def doc_confidence(document, norm=False):
 
     confidence = 0
 
@@ -36,13 +36,21 @@ def doc_confidence(document):
             if k in i:
                 confidence -= 1
 
+    if norm:
+        # The try is in case of division by zero
+        #  This can only happen with an empty document, so set score to 0.
+        try:
+            confidence = confidence / len(document.words)
+        except:
+            confidence = 0
+
     return confidence
 
 
-def folder_confidence(data_type, search_term):
+def folder_confidence(data_type, search_term, norm=False):
 
     docs = load_folder(data_type, search_term)
 
-    confidence = [doc_confidence(doc) for doc in docs]
+    confidence = [doc_confidence(doc, norm) for doc in docs]
 
     return confidence
